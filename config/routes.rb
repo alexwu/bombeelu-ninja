7ef965d9aa28382bf69f99b371b19b92734cec4e
Alex Wu <alexwu96@gmail.com>
2020-07-05 21:51:13 -0700

    Adds devise user and basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: "users/sessions" }
 
+  authenticated do
+    namespace :api, path: "/", constraints: { format: "json" } do
+
+    end
+
+    devise_scope :user do
+      root "logged_in#index", as: :authenticated_root
+      get "logout", to: "users/sessions#destroy", as: :destroy_user_session
+
+      scope "/", constraints: { format: "html" } do
+        get "/*path" => "logged_in#index", as: "authenticated_app"
+      end
+    end
+  end
+
+  get "login", to: "logged_out#index", as: :new_user_session
   root "home#index"
 end
